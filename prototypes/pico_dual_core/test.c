09f38fa4a3ae7d93b9da7bb2dd8e388c708e89d2
William Edward Cunningham <william.e.cunningham@maine.edu>
2023-11-05 14:27:58 -0500

    Using builtin ueue, not popping correctly

--- prototypes/pico_dual_core/test.c
+++ prototypes/pico_dual_core/test.c
@@ -3,17 +3,27 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
+#include "pico/util/queue.h"
 
 #include "custom_multicore.h"
 
 #define LED_PIN  25
 
 
+// built-in queue library help taken from https://arduino.stackexchange.com/questions/86671/rp2040-example-sketch-for-dual-core-queuing
+queue_t queue;
+
+typedef struct queueItem {
+  uint32_t value;
+} item;
+
+const int QUEUE_LENGTH = 128;
+
 int core1_main() {
     puts("hello from core 1\n");
-    while (1) {
-        sleep_ms(1000);
-        puts("core 1 is going\n");
+    struct queueItem temp;
+    while (queue_try_remove(&queue, &temp)) {
+        printf("Item popped from queue: %d\n", temp.value);
     }
 }
 
@@ -21,18 +31,38 @@ int main() {
     // initialize stdio for usb communication
     stdio_init_all();
 
+    sleep_ms(5000);
+
     // initialize the LED GPIO pin
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    // initialize the queue
+    queue_init(&queue, sizeof(item), QUEUE_LENGTH);
+
+    // start up the second core
     launch_core1((void *)core1_main);
 
+    uint32_t sequence[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    uint i = 0;
     while (1) {
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        sleep_ms(500);
         gpio_put(LED_PIN, 1);
         puts("Hello World\n");
-        sleep_ms(1000);
+        
+        struct queueItem temp;
+        temp.value = sequence[i % 10];
+        if (queue_try_add(&queue, &temp)) {
+            printf("Added %d to queue\n", temp.value);
+        }
+        else {
+            printf("Queue was full!\n");
+        }
+        sleep_ms(500);
+
+        i ++;
     }
 }
 
