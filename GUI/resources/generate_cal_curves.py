032c3a6bd2118c081be1df23d659978586fec4a5
William Edward Cunningham <william.e.cunningham@maine.edu>
2023-12-05 20:27:48 -0500

    Updated averaged calibration curve to truly fit to the average of the calibration curves

--- GUI/resources/generate_cal_curves.py
+++ GUI/resources/generate_cal_curves.py
@@ -39,28 +39,7 @@ if __name__ == "__main__":
     np.save("default_calibration_curves.npy", calibration.cal_curves_array, allow_pickle=False)
     print("Saved calibrations to default_calibration_curves.npy")
 
-
-    # also calculate an average calibration curve that can be applied to all sensors
-    data_files = Path("./raw_data").glob("*.csv")
-    avgs = []
-    pressures = []
-    for path in data_files:
-        avgs.append(np.average(np.loadtxt(path, dtype=np.uint8, delimiter=',').flatten()))
-        pressures.append(float(str(path).split('_')[2].split('pa')[0]))
-    
-    print(avgs, pressures)
-
-    params, cv = scipy.optimize.curve_fit(fit_function, avgs, pressures, p0=p0)
-
-    avg_cal_curves = np.empty_like(calibration.cal_curves_array)
-    for i in range(avg_cal_curves.shape[0]):
-        for j in range(avg_cal_curves.shape[1]):
-            avg_cal_curves[i, j] = np.asarray(params)
-
-    np.save("averaged_calibration_curves.npy", avg_cal_curves, allow_pickle=False)
-
-
-    f, axes = plt.subplots(nrows=1, ncols=1)
+    plt.figure(1)
 
     # plot the calibration curves
     x = np.linspace(0, 255, 100)
@@ -68,17 +47,48 @@ if __name__ == "__main__":
     for i in range(curves.shape[0]):
         for j in range(curves.shape[1]):
             params = curves[i, j]
-            axes.plot(x, calibration.fit_function(x, *params))
+            plt.plot(x, calibration.fit_function(x, *params))
 
-    axes.set_title("Calculated Calibration Curves")
-    axes.set_xlabel("Mat sensor reading (unitless)")
-    axes.set_ylabel("Mapped pressure value (Pa)")
-    axes.set_xlim([0, 210])
-    axes.set_ylim([-5, 100])
-    axes.axvline(207, color='red')
+    plt.title("Calculated Calibration Curves")
+    plt.xlabel("Mat sensor reading (unitless)")
+    plt.ylabel("Mapped pressure value (Pa)")
+    plt.xlim([0, 210])
+    plt.ylim([-5, 100])
+    plt.axvline(207, color='red')
 
-    f.tight_layout()
+    plt.figure(2)
+    # plt.plot(calibration.fit_function(x, *list(avg_cal_curves[0][0])))
+    plt.xlabel("Mat sensor reading (unitless)")
+    plt.ylabel("Mapped pressure value (Pa)")
+    plt.title("Averaged Calibration Curve")
 
+    # Lets also fit a vurve to the average of all the calibration curves to see if it is nicer overall
+    average_curve = []
+    for point in range(255):
+        point_sum = 0
+        n = 0
+        for i in range(curves.shape[0]):
+            for j in range(curves.shape[1]):
+                params = curves[i, j]
+                point_sum += calibration.fit_function(point, *params)
+                n += 1
+
+        average_curve.append(point_sum / n)
+
+    avg_params, cv = scipy.optimize.curve_fit(fit_function, list(range(255)), average_curve, p0=p0)
+
+    # save out the average cal curves
+    avg_cal_curves = np.empty_like(calibration.cal_curves_array)
+    for i in range(avg_cal_curves.shape[0]):
+        for j in range(avg_cal_curves.shape[1]):
+            avg_cal_curves[i, j] = np.asarray(avg_params)
+
+    np.save("averaged_calibration_curves.npy", avg_cal_curves, allow_pickle=False)
+
+
+    plt.plot([calibration.fit_function(i, *avg_params) for i in range(255)])
+    plt.xlim([0, 210])
+    plt.ylim([-5, 100])
     plt.show()
 
 
